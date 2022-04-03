40f21932c529124c5b67ad75f86990965eb7f050
Blatoy <lock.jeanneret@gmail.com>
2022-04-03 03:42:49 +0200

    Update fullscreen view

--- views/fullscreen/index.ts
+++ views/fullscreen/index.ts
@@ -1,1 +1,18 @@
- 
+import { Engine } from "engine/engine.js";
+import { GameTemplate } from "engine/game-template.js";
+
+let engine: Engine;
+const gameTemplate = new GameTemplate("/scripts/games/test-game");
+
+window.addEventListener("load", async () => {
+    await gameTemplate.load();
+    engine = new Engine("Engine 1", gameTemplate, document.getElementById("game") as HTMLDivElement);
+    engine.start();
+});
+
+document.addEventListener("keydown", async (e) => {
+    if (e.key === "r") {
+        await gameTemplate.reload();
+        engine.reloadGameTemplate();
+    }
+});
