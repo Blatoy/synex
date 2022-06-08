0bf7e91e53ed52ccd53d14aac20408819829f0fa
Blatoy <lock.jeanneret@gmail.com>
2022-06-09 00:09:04 +0200

    Add retrieving entity by name

--- engine/src/game-api/entities-api.ts
+++ engine/src/game-api/entities-api.ts
@@ -1,29 +1,30 @@
+import { State } from "frame-state.js";
 import { SerializedComponent } from "game-lib/types/component.js";
 import { GenericEntity } from "game-lib/types/entity.js";
 import { Entities } from "game-lib/types/game-api/entities.js";
-import { ComponentManager } from "game-lib/utils/component-manager.js";
+import { GameTemplate } from "game-template.js";
 import { MetaEntity } from "meta-entity.js";
 
 export class EntitiesAPI implements Entities {
     private entitiesToBeSpawned: GenericEntity[] = [];
+
+    constructor(public state: State) {}
+
     spawn(name: string, ...components: SerializedComponent[]): void {
         // TODO: Should this emit a global action?
         // ^ No if it's something deterministic (e.g. timed spawner, hitbox or something)
         // ^ Yes if it's done by something local (local spawn menu)
+        this.entitiesToBeSpawned.push(GameTemplate.entityFromDefinition(
+            { metadata: { name }, components }
+        ));
+    }
 
-        // TODO: Do not duplicate the code from game-template, add a function somewhere
-        const entity: GenericEntity = {
-            meta: new MetaEntity(
-                name,
-                components.map(serializedComponent => serializedComponent.Type)
-            )
-        };
-
-        components.forEach((component) => {
-            entity[component.Type.componentName] = ComponentManager.deserialize(component);
-        });
-
-        this.entitiesToBeSpawned.push(entity);
+    getByName(name: string) {
+        for (const entity of this.state.entities) {
+            if (entity.meta.name === name) {
+                return entity;
+            }
+        }
     }
 
     spawnQueuedEntities(entities: GenericEntity[]) {
@@ -41,5 +42,15 @@ export class EntitiesAPI implements Entities {
                 i--;
             }
         }
+    }
+
+    addQueuedComponents(entities: GenericEntity[]) {
+        /* for (let i = 0; i < entities.length; i++) {
+            const meta = entities[i].meta as MetaEntity;
+            if (meta.scheduledForDeletion) {
+                entities.splice(i, 1);
+                i--;
+            }
+        } */
     }
 }
--- game-lib/src/types/game-api/entities.ts
+++ game-lib/src/types/game-api/entities.ts
@@ -1,8 +1,10 @@
 import { SerializedComponent } from "types/component.js";
+import { GenericEntity } from "types/entity.js";
 
 export type Entities = {
     /**
      * Spawn (on next frame) an entity with a given list of components
      */
     spawn(name: string, ...components: SerializedComponent[]): void
+    getByName(name: string): GenericEntity | undefined
 }
