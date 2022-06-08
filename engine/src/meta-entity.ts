d67b1e527910946cc9dd91908969781fa5fdb64b
Blatoy <lock.jeanneret@gmail.com>
2022-06-09 00:09:47 +0200

    Add adding components

--- engine/src/meta-entity.ts
+++ engine/src/meta-entity.ts
@@ -1,10 +1,22 @@
-import { Component } from "game-lib/types/component.js";
-import { MetaEntity as MetaEntityInterface } from "game-lib/types/entity.js";
+import { Component, SerializedComponent } from "game-lib/types/component.js";
+import { GenericEntity, MetaEntity as MetaEntityInterface } from "game-lib/types/entity.js";
+import { GameTemplate } from "game-template.js";
 
 export class MetaEntity implements MetaEntityInterface {
     private _scheduledForDeletion = false;
+    private scheduleComponents: SerializedComponent[] = [];
 
-    constructor(public name: string, private _components: typeof Component[] = []) { }
+    constructor(
+        public name: string,
+        private _components: typeof Component[] = [],
+        private entity: GenericEntity
+    ) { }
+
+    addComponent(...components: SerializedComponent[]) {
+        for (const component of components) {
+            GameTemplate.addSerializedComponent(this.entity, component);
+        }
+    }
 
     delete() {
         this._scheduledForDeletion = true;
